94e84babf81c2b0fc0384a34c54f523fa17c9027
Berkay Kabay <mail@berkaykabay.de>
2019-08-30 12:05:16 +0200

    implementing validations on all models

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,7 @@
 class Destination < ApplicationRecord
   belongs_to :board, optional: :true
   belongs_to :user
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :description, length: { maximum: 300 }
 end
--- app/models/timeframe.rb
+++ app/models/timeframe.rb
@@ -1,4 +1,8 @@
 class Timeframe < ApplicationRecord
   belongs_to :board, optional: true
   belongs_to :user
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :description, length: { maximum: 100 }
 end
