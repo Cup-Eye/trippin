1154c76f08cb530e346019ed01f4efe18d9dc115
Berkay Kabay <mail@berkaykabay.de>
2019-08-30 13:48:36 +0200

    First version of validations completed

--- app/models/transportation.rb
+++ app/models/transportation.rb
@@ -2,11 +2,11 @@ class Transportation < ApplicationRecord
   belongs_to :board, optional: true
   belongs_to :user
 
-  validates :Route_number, presence: true
-  validates :booking_number, presence: true
+  validates :Route_number, presence: true, length: { maximum: 15 }
+  validates :booking_number, presence: true, length: { maximum: 15 }
   validates :departure_time, presence: true
   validates :arrival_time, presence: true
-  validates :departure_location, presence: true
-  validates :arrival_location, presence: true
+  validates :departure_location, presence: true, length: { maximum: 25 }
+  validates :arrival_location, presence: true, length: { maximum: 25 }
   validates :kind, presence: true
 end
