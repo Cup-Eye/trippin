abc53a50410b42e783bf6baf5a6d996a0b2c4568
Maurice Mohn <maurice_mohn@msn.com>
2019-09-02 19:31:23 +0200

    implement side button and restucture

--- app/controllers/timeframes_controller.rb
+++ app/controllers/timeframes_controller.rb
@@ -2,6 +2,7 @@ class TimeframesController < ApplicationController
   def index
     @board = Board.find(params[:timeframe_board_id])
     @timeframes = @board.timeframes
+    @trip = @board.trip
     if @timeframes.any?
       @earliest_date = @timeframes.order(start_date: :asc).first.start_date
       @last_date = @timeframes.order(end_date: :desc).first.end_date
