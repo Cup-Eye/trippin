2e909cac05ceb4d7604d449235052d676725d5e7
Berkay Kabay <mail@berkaykabay.de>
2019-09-02 17:42:06 +0200

    working voting system

---
+++ app/controllers/accommodation_votes_controller.rb
@@ -0,0 +1,20 @@
+class AccommodationVotesController < ApplicationController
+
+  def create
+    @accommodation = Accommodation.find(params[:accommodation_id])
+    @accommodation.liked_by current_user
+    redirect_to accommodation_board_accommodations_path(@accommodation.board)
+  end
+
+  def destroy
+    @accommodation = Accommodation.find(params[:accommodation_id])
+    @accommodation.unliked_by current_user
+    redirect_to accommodation_board_accommodations_path(@accommodation.board)
+  end
+
+  private
+
+  def accommodation_params
+    params.require(:accommodation).permit(:url, :name, :kind, :address, :price)
+  end
+end
