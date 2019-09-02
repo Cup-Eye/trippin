e792da0adceb9e07fc624d2fa6efa5759acc8e19
Klara Adams <klara.adams@mail.com>
2019-09-02 18:28:23 +0200

    added participants controller & model

---
+++ app/controllers/participants_controller.rb
@@ -0,0 +1,31 @@
+class ParticipantsController < ApplicationController
+  def new
+    @participant = Participant.new
+    @trip = Trip.find(params[:trip_id])
+  end
+
+  def create
+    @user = User.find_by(email: participant_params[:email])
+    @participant = Participant.new
+    @trip = Trip.find(params[:trip_id])
+    @participant.trip = @trip
+    @participant.user = @user
+    if @participant.save
+      redirect_to new_trip_participant_path(@trip)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @participant = Participant.find(params[:id])
+    @participant.destroy
+    redirect_to
+  end
+
+  private
+
+  def participant_params
+    params.require(:participant).permit(:email)
+  end
+end
