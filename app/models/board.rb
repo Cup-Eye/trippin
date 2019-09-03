688062eea53a2b7df28ec4c68e2cc63c496b7908
Anne Rohde <rohde.anch@gmail.com>
2019-09-03 10:48:33 +0200

    delete trip when commemnts

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,6 @@
 class Board < ApplicationRecord
   belongs_to :trip
-  has_many :comments, foreign_key: :board_id, class_name: "Comment"
+  has_many :comments, foreign_key: :board_id, class_name: "Comment", dependent: :destroy
 
   def name
     type.gsub("Board", "")
