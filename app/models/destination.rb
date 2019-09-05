e965b0765cdbcd7e3c32938a9906c2158b1b691a
Anne Rohde <rohde.anch@gmail.com>
2019-09-05 17:21:12 +0200

    photo uploader destination and photo view in destination and button on participant

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -3,6 +3,8 @@ class Destination < ApplicationRecord
   belongs_to :board, optional: :true
   belongs_to :user
 
+   mount_uploader :photo, PhotoUploader
+
   validates :name, presence: true, uniqueness: { scope: :board }, length: { minimum: 3 }
   validates :description, length: { maximum: 300 }
 end
