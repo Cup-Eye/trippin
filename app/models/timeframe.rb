153c8d7ebe7e461733d14c4d92a29408219d9641
Anne Rohde <rohde.anch@gmail.com>
2019-09-05 22:05:00 +0200

    validation timeframe change to back

--- app/models/timeframe.rb
+++ app/models/timeframe.rb
@@ -7,17 +7,17 @@ class Timeframe < ApplicationRecord
   validates :description, length: { maximum: 100 }
   # validate :cannot_overlap_another_timeframe
 
-  # def cannot_overlap_another_timeframe
-  #   condition_on_create = user.timeframes.where('start_date <= ?', start_date).where('end_date >= ?', start_date).any? ||
-  #     user.timeframes.where('start_date <= ?', end_date).where('end_date >= ?', end_date).any? ||
-  #     user.timeframes.where('start_date <= ?', end_date).where('end_date >= ?', start_date).any?
-  #   condition_on_update = user.timeframes.where('start_date <= ?', start_date).where('end_date >= ?', start_date).where.not(id: id).any? ||
-  #     user.timeframes.where('start_date <= ?', end_date).where('end_date >= ?', end_date).where.not(id: id).any? ||
-  #     user.timeframes.where('start_date <= ?', end_date).where('end_date >= ?', start_date).where.not(id: id).any?
+  def cannot_overlap_another_timeframe
+    condition_on_create = user.timeframes.where('start_date <= ?', start_date).where('end_date >= ?', start_date).any? ||
+      user.timeframes.where('start_date <= ?', end_date).where('end_date >= ?', end_date).any? ||
+      user.timeframes.where('start_date <= ?', end_date).where('end_date >= ?', start_date).any?
+    condition_on_update = user.timeframes.where('start_date <= ?', start_date).where('end_date >= ?', start_date).where.not(id: id).any? ||
+      user.timeframes.where('start_date <= ?', end_date).where('end_date >= ?', end_date).where.not(id: id).any? ||
+      user.timeframes.where('start_date <= ?', end_date).where('end_date >= ?', start_date).where.not(id: id).any?
 
-  #   if persisted? ? condition_on_update : condition_on_create
-  #     errors.add(:start_date, "overlaps another timeframe")
-  #   end
-  # end
+    if persisted? ? condition_on_update : condition_on_create
+      errors.add(:start_date, "overlaps another timeframe")
+    end
+  end
 end
 
